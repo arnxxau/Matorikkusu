75f8f6494d778b4a4f0963ad7d9c8850b5da56d4
arnxxau <arnxxau@gmail.com>
2021-09-01 21:43:09 +0200

    library fix-3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.30"
     `java-library`
-    maven
+    `maven-publish`
 }
 
 group = "com.github.arnxxau"
